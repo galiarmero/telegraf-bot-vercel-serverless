42609ea77f3f59dccf4464676f7a8a8e9fe24a25
Gali Armero <galiarmero.dev@gmail.com>
2022-09-02 18:07:28 +0800

    Tweak index page message

--- api/serverless.js
+++ api/serverless.js
@@ -5,8 +5,11 @@ const app = Fastify({ logger: true })
 app.register(require('../src/bot'))
 app.register(require('../src/webhook'))
 app.get('/',  (req, rep) => rep.send(
-  'Hi! I am a Telegram bot server 🤖\n' +
-  'The webhook is hidden in a secret path that only the bot knows 🦾'
+  'Hi! This is a Telegram bot server 🤖\n' +
+  '📣 Built with Telegraf\n' +
+  '⚡ Spun up using Fastify\n' +
+  '🚀 Hosted on Vercel\n' +
+  'The webhook is hidden in a secret path that only Telegram knows 🦾'
 ))
 
 module.exports = async (req, res) => {
